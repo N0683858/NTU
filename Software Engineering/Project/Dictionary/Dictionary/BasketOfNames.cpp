7cfb290b7c20ea0d7eaa3b2159f2b808e1fbf73f
Hamid Mujtaba <N0683858@my.ntu.ac.uk>
2020-02-24 00:50:32 +0000

    update on soft eng

--- Software Engineering/Project/Dictionary/Dictionary/BasketOfNames.cpp
+++ Software Engineering/Project/Dictionary/Dictionary/BasketOfNames.cpp
@@ -2,12 +2,52 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
+BasketOfNames::BasketOfNames()
+{
+	readFile_intoMap("");
+}
 
-std::unordered_map<std::string, std::string>
-listOfNames = {
-		{"RED","#FF0000"},
-		{"GREEN","#00FF00"},
-		{"BLUE","#0000FF"}
-};
+void BasketOfNames::insert(Name name, nameToWest westernName)
+{
+	listOfNames[name] = westernName;
+}
+
+BasketOfNames::nameToWest BasketOfNames::getPersonToWest(Name)
+{
+	return NULL;
+}
+
+void BasketOfNames::remove(Name)
+{
+
+}
+
+void BasketOfNames::readFile_intoMap(std::string file)
+{
+	Name tempName;
+	nameToWest tempWestName;
+	string line;
+
+	ifstream namesDataFile(file);
+	if (namesDataFile.is_open())
+	{
+		while (getline(namesDataFile, line))
+		{
+			stringstream ss(line);
+			getline(ss, tempName, ',');
+			getline(ss, tempWestName, ',');
+
+			insert(tempName, tempWestName);
+		}
+	}
+	else
+	{
+		cout << "Error opening file!" << endl;
+	}
+}
+
 
--- Software Engineering/Project/Dictionary/Dictionary/BasketOfNames.h
+++ Software Engineering/Project/Dictionary/Dictionary/BasketOfNames.h
@@ -1,10 +1,76 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <fstream>
+#include <sstream>
 
 class BasketOfNames
 {
+public:
+	using Name = std::string;
+	using nameToWest = std::string;
 
-	//std::unordered_map<std::string, std::string> listOfNames;
+	BasketOfNames();
+	void insert(Name, nameToWest);
+	nameToWest getPersonToWest(Name);
+	void remove(Name);
+	void readFile_intoMap(std::string);
+	void readResultData_intoList();
+
+
+private:
+	std::unordered_map<Name, nameToWest> listOfNames;
+	std::list<std::string> resultData;
+
+
 };
 
+//BasketOfNames<Name, nameToWest>::BasketOfNames()
+//{
+//	ReadFile_intoMap("");
+//}
+//
+//template<typename Name, typename nameToWest>
+//void BasketOfNames<Name, nameToWest>::insert(Name name, nameToWest westernName)
+//{
+//	listOfNames[name] = westernName;
+//}
+//
+//template<typename Name, typename nameToWest>
+//nameToWest BasketOfNames<Name, nameToWest>::getPersonToWest(Name)
+//{
+//	return NULL;
+//}
+//
+//template<typename Name, typename nameToWest>
+//void BasketOfNames<Name, nameToWest>::remove(Name)
+//{
+//
+//}
+//
+//template<typename Name, typename nameToWest>
+//void BasketOfNames<Name, nameToWest>::ReadFile_intoMap(std::string file)
+//{
+//Name tempName;
+//nameToWest tempWestName;
+//string line;
+//
+//ifstream namesDataFile(file);
+//if (namesDataFile.is_open())
+//{
+//	while (getline(namesDataFile, line))
+//	{
+//		stringstream ss(line);
+//		getline(ss, tempName, ',');
+//		getline(ss, tempWestName, ',');
+//
+//		insert(tempName, tempWestName);
+//	}
+//}
+//else
+//{
+//	cout << "Error opening file!" << endl;
+//}
+//
+//	
+//}
